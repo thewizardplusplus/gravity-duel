91fc67c083c0e3e873aba40ecdacf88e0cb23761
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-05 23:00:00 +0300

    Add the physics.make_circle_collider() function

--- physics.lua
+++ physics.lua
@@ -28,6 +28,26 @@ function physics.make_rectangle_collider(world, kind, rectangle)
 end
 
 ---
+-- @tparam windfield.World world
+-- @tparam "static"|"dynamic" kind
+-- @tparam number x [0, ∞)
+-- @tparam number y [0, ∞)
+-- @tparam number radius [0, ∞)
+-- @treturn windfield.Collider
+function physics.make_circle_collider(world, kind, x, y, radius)
+  assert(type(world) == "table")
+  assert(kind == "static" or kind == "dynamic")
+  assert(typeutils.is_positive_number(x))
+  assert(typeutils.is_positive_number(y))
+  assert(typeutils.is_positive_number(radius))
+
+  local collider = world:newCircleCollider(x, y, radius)
+  collider:setType(kind)
+
+  return collider
+end
+
+---
 -- @tparam {windfield.Collider,...} colliders
 -- @tparam func handler func(collider: windfield.Collider): nil
 function physics.process_colliders(colliders, handler)
